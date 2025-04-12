af386b6a2afe5b530ab6e4f1f580d2a204615c78
jslim0602 <jslim0602@gmail.com>
2025-04-12 23:05:08 +0900

    Widget trait implemented for SevenDigit, Dot, TimePrinter(pub)

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,55 @@
-use std::{io::{stdout, Stdout}, time::Duration};
+use std::{io::Stdout, time::Duration};
 use crossterm::{
-    execute,
     cursor::MoveTo,
     ExecutableCommand,
     style::Print,
 };
 
+pub trait Widget {
+    type PrintingData;
+    fn get_size(&self) -> [u16; 2];
+    fn show(&self, stdout: &mut Stdout, data: &Self::PrintingData);
+    fn unshow(&self, stdout: &mut Stdout);
+    fn move_and_unshow(&mut self, stdout: &mut Stdout, p_col: u16, p_row: u16);
+}
+
 
 // ========== SevenDigit ==========
 struct SevenDigit {
     p_col: u16, p_row: u16,
-    size: u16,
+    line_length: u16,
 }
 
 impl SevenDigit {
-    fn new(p_col: u16, p_row: u16, size: u16) -> SevenDigit {
-        SevenDigit {p_col, p_row, size}
+    fn new(p_col: u16, p_row: u16, line_length: u16) -> SevenDigit {
+        SevenDigit {p_col, p_row, line_length}
     }
+}
+
+impl Widget for SevenDigit {
+    type PrintingData = u64;
 
     fn get_size(&self) -> [u16; 2] {
         // return [col, row]
-        [self.size+2, self.size*2+1]
+        [self.line_length+2, self.line_length*2+1]
     }
 
     fn unshow(&self, stdout: &mut Stdout) {
-        let seg_p_col: [u16;8] = [0, 1, 0, 1,         self.size+1, 0,           1,           self.size+1];
-        let seg_p_row: [u16;8] = [0, 0, 1, self.size, 1,           self.size+1, self.size*2, self.size+1];
+        let seg_p_col: [u16;8] = [0, 1, 0, 1,                self.line_length+1, 0,                  1,                  self.line_length+1];
+        let seg_p_row: [u16;8] = [0, 0, 1, self.line_length, 1,                  self.line_length+1, self.line_length*2, self.line_length+1];
         let seg_is_col: u8 = 0b01010010;
 
         for i in 1..8 {
             // check this segment's direction and print
-            if (seg_is_col & (1<<(7-i)) > 0) {
-                for j in 0..self.size {
+            if seg_is_col & (1<<(7-i)) > 0 {
+                for j in 0..self.line_length {
                     stdout
                         .execute(MoveTo(self.p_col+seg_p_col[i]+j, self.p_row+seg_p_row[i])).unwrap()
                         .execute(Print(' ')).unwrap();
                 }
             }
             else {
-                for j in 0..self.size {
+                for j in 0..self.line_length {
                     stdout
                         .execute(MoveTo(self.p_col+seg_p_col[i], self.p_row+seg_p_row[i]+j)).unwrap()
                         .execute(Print(' ')).unwrap();
@@ -47,7 +58,7 @@ impl SevenDigit {
         }
     }
 
-    fn show(&self, stdout: &mut Stdout, num: u64) {
+    fn show(&self, stdout: &mut Stdout, data: &Self::PrintingData) {
         /*
          1       _
         234     |_|
@@ -55,10 +66,10 @@ impl SevenDigit {
         */
         
         // index: [ignore, 1, 2, 3, ..., 7], 0b01234567
-        let seg_p_col: [u16;8] = [0, 1, 0, 1,         self.size+1, 0,           1,           self.size+1];
-        let seg_p_row: [u16;8] = [0, 0, 1, self.size, 1,           self.size+1, self.size*2, self.size+1];
+        let seg_p_col: [u16;8] = [0, 1, 0, 1,                self.line_length+1, 0,                  1,                  self.line_length+1];
+        let seg_p_row: [u16;8] = [0, 0, 1, self.line_length, 1,                  self.line_length+1, self.line_length*2, self.line_length+1];
         let seg_is_col: u8 = 0b01010010;
-        let on_off: u8 = match num {
+        let on_off: u8 = match data {
             0 => 0b01101111,
             1 => 0b00001001,
             2 => 0b01011110,
@@ -75,28 +86,35 @@ impl SevenDigit {
         for i in 1..8 {
             // decide what should be printed on this segment
             let to_print: char = 
-            if (on_off & (1<<(7-i)) == 0) {' '} 
+            if on_off & (1<<(7-i)) == 0 {' '} 
             else {
-                if (seg_is_col & (1<<(7-i)) > 0)    {'_'}
+                if seg_is_col & (1<<(7-i)) > 0      {'_'}
                 else                                {'|'}
             };
 
             // check this segment's direction and print
-            if (seg_is_col & (1<<(7-i)) > 0) {
-                for j in 0..self.size {
+            if seg_is_col & (1<<(7-i)) > 0 {
+                for j in 0..self.line_length {
                     stdout
                         .execute(MoveTo(self.p_col+seg_p_col[i]+j, self.p_row+seg_p_row[i])).unwrap()
                         .execute(Print(to_print)).unwrap();
                 }
             }
             else {
-                for j in 0..self.size {
+                for j in 0..self.line_length {
                     stdout
                         .execute(MoveTo(self.p_col+seg_p_col[i], self.p_row+seg_p_row[i]+j)).unwrap()
                         .execute(Print(to_print)).unwrap();
                 }
             }
         }
+    }
+
+    fn move_and_unshow(&mut self, stdout: &mut Stdout, p_col: u16, p_row: u16) {
+        self.unshow(stdout);
+        
+        self.p_col = p_col;
+        self.p_row = p_row;
     }
 }
 
@@ -110,6 +128,14 @@ impl Dot {
     fn new(p_col: u16, p_row: u16) -> Dot {
         Dot { p_col, p_row }
     }
+}
+
+impl Widget for Dot {
+    type PrintingData = char;
+
+    fn get_size(&self) -> [u16; 2] {
+        [1, 1]
+    }
 
     fn unshow(&self, stdout: &mut Stdout) {
         stdout
@@ -117,10 +143,17 @@ impl Dot {
             .execute(Print(' ')).unwrap();
     }
 
-    fn show(&self, stdout: &mut Stdout) {
+    fn show(&self, stdout: &mut Stdout, _data: &Self::PrintingData) {
         stdout
             .execute(MoveTo(self.p_col, self.p_row)).unwrap()
             .execute(Print('.')).unwrap();
+    }
+
+    fn move_and_unshow(&mut self, stdout: &mut Stdout, p_col: u16, p_row: u16) {
+        self.unshow(stdout);
+
+        self.p_col = p_col;
+        self.p_row = p_row;
     }
 }
 
@@ -128,30 +161,30 @@ impl Dot {
 // ========== TimePrinter ==========
 pub struct TimePrinter {
     p_col: u16, p_row: u16,
-    size: u16, space: u16,
+    line_length: u16, space: u16,
 
     digits: Vec<SevenDigit>,
-    dots: Vec<Dot>
+    dots: Vec<Dot>,
 }
 
 impl TimePrinter {
-    pub fn new(p_col: u16, p_row: u16, size: u16, space: u16) -> TimePrinter {
-        let seg_col_size = size+2;
-        let seg_row_size = size*2+1;
+    pub fn new(p_col: u16, p_row: u16, line_length: u16, space: u16) -> TimePrinter {
+        let seg_col_size = line_length+2;
+        let seg_row_size = line_length*2+1;
 
-        TimePrinter { p_col, p_row, size, space, 
+        TimePrinter { p_col, p_row, line_length, space, 
             digits: vec![
                 // hour
-                SevenDigit::new(p_col, p_row, size),
-                SevenDigit::new(p_col+seg_col_size, p_row, size),
+                SevenDigit::new(p_col, p_row, line_length),
+                SevenDigit::new(p_col+seg_col_size, p_row, line_length),
 
                 // minute
-                SevenDigit::new(p_col+seg_col_size*2+space, p_row, size),
-                SevenDigit::new(p_col+seg_col_size*3+space, p_row, size),
+                SevenDigit::new(p_col+seg_col_size*2+space, p_row, line_length),
+                SevenDigit::new(p_col+seg_col_size*3+space, p_row, line_length),
 
                 // second
-                SevenDigit::new(p_col+seg_col_size*4+space*2, p_row, size),
-                SevenDigit::new(p_col+seg_col_size*5+space*2, p_row, size),
+                SevenDigit::new(p_col+seg_col_size*4+space*2, p_row, line_length),
+                SevenDigit::new(p_col+seg_col_size*5+space*2, p_row, line_length),
             ],
 
             dots: vec![
@@ -165,15 +198,19 @@ impl TimePrinter {
             ]
         }
     }
+}
 
-    pub fn get_size(&self) -> [u16; 2] {
+impl Widget for TimePrinter {
+    type PrintingData = Duration;
+
+    fn get_size(&self) -> [u16; 2] {
         let seg_size: [u16; 2] = self.digits[0].get_size();
 
         [seg_size[0]*6+self.space*2, seg_size[1]]
     }
 
 
-    pub fn unshow(&self, stdout: &mut Stdout) {
+    fn unshow(&self, stdout: &mut Stdout) {
         // unshow dots
         for u in 0..4usize {
             self.dots[u].unshow(stdout);
@@ -185,21 +222,27 @@ impl TimePrinter {
         }
     }
 
-    pub fn show(&self, stdout: &mut Stdout, current_time: Duration) {
+    fn show(&self, stdout: &mut Stdout, data: &Self::PrintingData) {
         // current_time: UNIX_EPOCH ~ now
 
-        let t: u64 = current_time.as_secs()%(3600*24);
+        let t: u64 = data.as_secs()%(3600*24);
         let t_digit: [u64; 6] = [t/36000, (t/3600)%10, (t%3600)/600, ((t%3600)/60)%10, (t%60)/10, (t%60)%10];
-        // 0~1: hour, 2~3: minute, 4~5: second
 
         // show dots
         for u in 0..4usize {
-            self.dots[u].show(stdout);
+            self.dots[u].show(stdout, &'.');
         }
 
         // show digits
         for u in 0..6usize {
-            self.digits[u].show(stdout, t_digit[u]);
+            self.digits[u].show(stdout, &t_digit[u]);
         }
+    }
+
+    fn move_and_unshow(&mut self, stdout: &mut Stdout, p_col: u16, p_row: u16) {
+        self.unshow(stdout);
+        
+        self.p_col = p_col;
+        self.p_row = p_row;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 // libraries
 use std::{
-        io::{stdout, Write}, thread::sleep, time::{Duration, SystemTime, UNIX_EPOCH}
+        io::stdout, thread::sleep, time::{Duration, SystemTime, UNIX_EPOCH}
     };
 
 use crossterm::{
-    cursor::{MoveTo, MoveToNextLine}, 
-    event::{self, poll, Event, KeyCode}, 
-    execute, 
-    // style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor}, 
-    terminal::{disable_raw_mode, enable_raw_mode, size, SetSize, Clear, ClearType}, 
-    ExecutableCommand
+    cursor::MoveToNextLine, event::{self, poll, Event, KeyCode}, execute, style::{Print, ResetColor, SetForegroundColor}, terminal::{disable_raw_mode, enable_raw_mode, size, SetSize}, ExecutableCommand
 };
-use ui::TimePrinter;
 
-mod ui;
+use tui_program::Widget;
+use tui_program::TimePrinter;
 
+
+// ========== raw terminal ==========
 fn in_raw() -> std::io::Result<()> {
     
     let (cols, rows) = size()?;
@@ -31,17 +28,21 @@ fn in_raw() -> std::io::Result<()> {
 
     // enable raw mode
     enable_raw_mode().unwrap();
-    // execute!(stdout(), MoveToNextLine(1))?;
     
-    let mut stdout = stdout();
-    let mut t = Duration::new(0, 0);
-    let clock = TimePrinter::new(2, 2, 3, 3);
+    // set output
+    let mut binding = stdout();
+    let mut s_out = binding
+        .execute(SetForegroundColor(crossterm::style::Color::Rgb{r: 225, g: 104, b: 5})).unwrap();
+
+    // set clock
+    let mut current_time = Duration::new(0, 0);
+    let clock = TimePrinter::new(2, 2, 6, 3);
 
     loop {
         // show time
-        t = SystemTime::now().duration_since(UNIX_EPOCH).unwrap()
+        current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap()
                 +Duration::from_secs(3600*9);       // KST: UTC+09:00
-        clock.show(&mut stdout, t);
+        clock.show(&mut s_out, &current_time);
 
         // listen event
         if poll(Duration::from_millis(100)).unwrap() {
@@ -63,11 +64,16 @@ fn in_raw() -> std::io::Result<()> {
         sleep(Duration::from_millis(50));
     }
 
-    clock.unshow(&mut stdout);
-    execute!(stdout, MoveToNextLine(1))?;
+    // clean console
+    clock.unshow(&mut s_out);
+
+    s_out
+        .execute(ResetColor).unwrap()
+        .execute(MoveToNextLine(1)).unwrap()
+        .execute(Print("close")).unwrap();
 
     disable_raw_mode().unwrap();
-    println!("out");
+    
 
     return Ok(());
 }
